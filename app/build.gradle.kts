6aa53b0781d71ba84643190476fb33f3b0752630
BS <bsayy072@uottawa.ca>
2025-11-09 17:36:12 -0500

    Firebase integration
    google-services.json contains an API-key, but these are public (non-secret) according to the documentation. You use the security rules to provide security

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,6 +39,10 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-functions")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/personalfoodlogapp/PersonalFoodApplication.java
+++ app/src/main/java/com/example/personalfoodlogapp/PersonalFoodApplication.java
@@ -2,6 +2,9 @@ package com.example.personalfoodlogapp;
 
 import android.app.Application;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,14 +19,13 @@ public class PersonalFoodApplication extends Application {
     // So whenever its killed, requiresReload is set to true and we need to grab data from the server
     public boolean requiresReload = true;
 
-    // These 2 are the same irrespective of the selected date
+    // These are the same irrespective of the selected date
     public int monthlyGoalsMet;
     public int monthlyGoalsPossible;
 
-    // This data depends on the selected date
-    public LocalDate currentDate;
-    public int calorieGoal;
-    public ArrayList<Pair<String, Integer>> foodItems;
+    public FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public FirebaseAuth auth = FirebaseAuth.getInstance();
+
 
     // Table of macros... Listed per gram (Calorie, Sodium, Fat, Sugar)
     public Map<String, ArrayList<Double>> foodMacroMap = new HashMap<>() {{
@@ -31,6 +33,11 @@ public class PersonalFoodApplication extends Application {
         put("Tomato", new ArrayList<>(Arrays.asList(0.18, 0.00005, 0.002, 0.026)));
         put("Lettuce", new ArrayList<>(Arrays.asList(0.15, 0.0003, 0.001, 0.012)));
     }};
+
+    // This data depends on the selected date
+    public LocalDate currentDate;
+    public int calorieGoal;
+    public ArrayList<Pair<String, Integer>> foodItems;
 
     public void onCreate() {
         super.onCreate();
@@ -69,8 +76,16 @@ public class PersonalFoodApplication extends Application {
         calorieGoal = 1000;
     }
 
+    public void attemptLogin() {
+        // TO BE IMPLEMENTED: Allow login
+    }
+
     public void sendDataToServer(LocalDate desiredDataDate) {
-        // TO BE IMPLEMENTED
+        // TO BE IMPLEMENTED: Write data to server
+    }
+
+    public void useAIFoodDetection() {
+        // TO BE IMPLEMENTED: Run code on server
     }
 
 }
